b1afbc17844414b68f745f89efdec101090f365b
NoLuckIngame <n.j@outlook.de>
2025-10-12 16:07:37 +0200

    update

---
+++ Kitchen Nightmare/Assets/scripts/PlayerAnimator.cs
@@ -0,0 +1,19 @@
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+    [SerializeField] private player player;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();       
+    }
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
+    
